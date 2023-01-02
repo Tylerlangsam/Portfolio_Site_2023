6822e6b34b3384ee1fdd64b4a0d7c700d223b7fb
Tylerlangsam <90009414+Tylerlangsam@users.noreply.github.com>
2023-01-02 13:19:22 -0800

    Creating contact form

--- Portfolio/home/views.py
+++ Portfolio/home/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpRequest
 
 # Create your views here.
 
@@ -8,4 +11,22 @@ def home(request):
 
 
 def contact(request):
-    return render(request, 'home/contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = "Inquiry"
+            body = {
+                'first_name': form.cleaned_data['first_name'],
+                'last_name': form.cleaned_data['last_name'],
+                'email': form.cleaned_data['email_address'],
+                'message': form.cleaned_data['message']
+            }
+            message = "\n".join(body.values())
+
+        try:
+            send_mail(subject, message, 'admin@example.com', ['admin@example.com'])
+        except BadHeaderError:
+            return HttpRequest('Invalid header found.')
+        return redirect('home/index.html')
+    form = ContactForm()
+    return render(request, 'home/contact.html', {'form':form})
