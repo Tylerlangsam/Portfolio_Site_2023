4283b38c89af172b650ebe88395566ac22c3332f
Tylerlangsam <90009414+Tylerlangsam@users.noreply.github.com>
2023-01-06 15:29:26 -0800

    forms

--- Portfolio/home/forms.py
+++ Portfolio/home/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    first_name = forms.CharField(max_length=50)
-    last_name = forms.CharField(max_length=50)
-    email_address = forms.EmailField(max_length = 150)
-    message = forms.CharField(widget=forms.Textarea, max_length=2500)
+    first_name = forms.CharField(widget=forms.TextInput(attrs={ 'placeholder':'First Name', 'class':'input_text'}),max_length=50, label='')
+    last_name = forms.CharField(widget=forms.TextInput(attrs={ 'placeholder':'Last Name', 'class':'input_text'}),max_length=50, label='')
+    email_address = forms.EmailField(widget=forms.TextInput(attrs={ 'placeholder':'Email', 'class':'input_text'}), max_length = 150, label='')
+    message = forms.CharField(widget=forms.Textarea(attrs={ 'placeholder':'Send me a message', 'class':'message_text'}), max_length=2500, label='')
